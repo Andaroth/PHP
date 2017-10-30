dcd5d124f58f77a397437eb6fe8e05772de33128
Anda <ax.fiolle@gmail.com>
2017-10-30 11:04:14 +0100

    exo 1 css + better

--- index.php
+++ index.php
@@ -15,6 +15,14 @@ if (isset($_POST["child"]) && isset($_POST["instit"]) && isset($_POST["reason"])
 <html>
     <head>
         <title>Du PHP</title>
+        <meta charset="utf-8" />
+        <link href="https://fonts.googleapis.com/css?family=Indie+Flower%7CPacifico" rel="stylesheet"> 
+        <style>
+            * { font-family: Indie Flower; text-align: center; }
+            #motif { display: block; max-width: 200px; margin: 0 auto; text-align: left; }
+            h1, h2 { font-family: Pacifico; }
+            #output { text-align: left; border: 1px dashed grey; max-width: 50%; margin: 50px auto; padding: 25px;}
+        </style>
     </head>
     <body>
         <h1>Générateur de sorrynotsorry</h1>
@@ -25,7 +33,7 @@ if (isset($_POST["child"]) && isset($_POST["instit"]) && isset($_POST["reason"])
             <label for="instit">Le nom de l'instituteur/trice : </label>
             <input id="instit" name="instit" type="text" />
             <br/>
-            <div>
+            <div id="motif">
                 <p>Le motif de votre absence :</p>
                 <input name="reason" type="radio" value="sick" id="sick" checked/>
                 <label for="sick">Maladie</label><br/>
@@ -43,6 +51,31 @@ if (isset($_POST["child"]) && isset($_POST["instit"]) && isset($_POST["reason"])
             <?php 
             
                 if ($child != "" && $instit != "" && $motif != "") {
+                    $day = date("D");
+                    switch($day) {
+                        case "Mon":
+                            $day = "lundi";
+                            break;
+                        case "Tue":
+                            $day = "mardi";
+                            break;
+                        case "Wed":
+                            $day = "mercredi";
+                            break;
+                        case "Thu":
+                            $day = "jeudi";
+                            break;
+                        case "Fri":
+                            $day = "vendredi";
+                            break;
+                        case "Sat":
+                            $day = "samedi";
+                            break;
+                        case "Sun":
+                            $day = "dimanche";
+                            break;
+                    }
+                    $date = $day." ".date("d/m/Y");
                     switch($motif) {
                         case "sick":
                             $motif = "Il/Elle avait le nez qui coule sévère et son cerveau qui sort par les oreilles.";
@@ -56,11 +89,11 @@ if (isset($_POST["child"]) && isset($_POST["instit"]) && isset($_POST["reason"])
                         case "poney":
                             $motif = "Il/Elle devait chevaucher son destrier naval pour mettre la baballe dans le filet.";
                             break;
-                        
                     }
                     echo "<h2>Votre motif d'excuse est le suivant</h2>";
-                    echo "Votre sale gamin de m*rde nommé <strong>".$child."</strong> est absent de la classe de <strong>".$instit."</strong> ! <br/>";
-                    echo "Le motif est très simple : <strong>".$motif."</strong>";
+                    echo "Bonjour Monsieur/Madame <strong>".$instit."</strong>, <br/>Notre sale gamin de m*rde nommé <strong>".$child."</strong> sera absent ce ".$date." de votre classe qui poue les pieds ! <br/>";
+                    echo "Le motif est très simple : <strong>".$motif."</strong><br/>";
+                    echo "Merci de votre compréhension, <br/>Les parents de ".$child;
                 }
             
             ?>
