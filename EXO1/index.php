a37e5e16a97ea9eade7ccee1420834d79854d194
Anda <ax.fiolle@gmail.com>
2017-10-30 12:04:02 +0100

    Some UX improvement

--- EXO1/index.php
+++ EXO1/index.php
@@ -19,10 +19,10 @@ if (isset($_POST["child"]) && isset($_POST["instit"]) && isset($_POST["reason"])
         <h1>Générateur de sorrynotsorry</h1>
         <form action="index.php" method="post">
             <label for="child">Le nom de votre enfant : </label>
-            <input id="child" name="child" type="text" />
+            <input id="child" name="child" type="text" autocomplete="off" />
             <br/>
             <label for="instit">Le nom de l'instituteur/trice : </label>
-            <input id="instit" name="instit" type="text" />
+            <input id="instit" name="instit" type="text" autocomplete="off" />
             <br/>
             <div id="motif">
                 <p>Le motif de votre absence :</p>
@@ -47,20 +47,23 @@ if (isset($_POST["child"]) && isset($_POST["instit"]) && isset($_POST["reason"])
                     case "Thu": $day = "jeudi"; break;
                     case "Fri": $day = "vendredi"; break;
                     case "Sat": $day = "samedi"; break;
-                    case "Sun": $day = "dimanche"; break; }
+                    case "Sun": $day = "dimanche"; break; 
+                    default: $day = "jour du triche"; break; }
                 $date = $day." ".date("d/m/Y"); // On va ajouter la date à la suite du jour
                 switch($motif) { // En fonction du motif qui a été coché, on va changer le texte de sortie
                     case "sick": $motif = "Il/Elle avait le nez qui coule sévère et son cerveau qui sort par les oreilles."; break;
                     case "deadcat": $motif = "Petit bébé chat nous a quitté, offrez-nous du Nutella."; break;
                     case "extra": $motif = "Il/Elle devait photographier des avions pour prouver l'existence des chemtrails."; break;
                     case "poney": $motif = "Il/Elle devait chevaucher son destrier naval pour mettre la baballe dans le filet."; break;
+                    default: $motif = "Nous avons cheaté le formulaire, nous sommes des sous-humains"; break;
                 } // À la fin des switch, je vais pouvoir enfin echo le mot d'excuse en html en rappelant mes variables définies précédemment
                 echo "<div id=\"output\">";
                 echo "<h2>Votre motif d'excuse</h2>";
-                echo "Bonjour Monsieur/Madame <strong>".$instit."</strong>, <br/>Notre sale gamin de m*rde nommé <strong>".$child."</strong> sera absent ce ".$date." de votre classe qui pue les pieds ! <br/>";
+                echo "Bonjour Monsieur/Madame <strong>".$instit."</strong>, <br/>Notre turbulent bambin nommé <strong>".$child."</strong> sera absent ce ".$date." de votre classe qui pue les pieds ! <br/>";
                 echo "Le motif est très simple : <strong>".$motif."</strong><br/>";
                 echo "Merci de votre compréhension, <br/>Les parents de ".$child;
-                echo "</div>"; }
+                echo "</div>"; 
+            } // Fin du if (params != "")
         ?>
     </body>
 </html>
